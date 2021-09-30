656e630417843b13007a877dbbc813c9acca88d3
joy.lazari@gmail.com <joy.lazari@gmail.com>
2021-09-30 09:15:26 +0200

    Add default sender and refactor send args names

--- src/Api.php
+++ src/Api.php
@@ -4,6 +4,7 @@ namespace Velostazione\BeSMS;
 
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\GuzzleException;
+use Velostazione\BeSMS\Exceptions\InvalidRecipientError;
 use Velostazione\BeSMS\Exceptions\MessageNotEnqueuedError;
 use Velostazione\BeSMS\Exceptions\SendError;
 
@@ -20,35 +21,40 @@ final class Api
         string $password,
         string $apiId,
         string $reportType = 'C',
+        string $sender = null
     )
     {
         $this->defaultParams = [
             'authlogin'   => $username,
             'authpasswd'  => $password,
             'id_api'      => $apiId,
-            'report_type' => $reportType
+            'report_type' => $reportType,
+            'sender'      => $sender ? base64_encode($sender) : null
         ];
         $this->client = $client;
     }
 
     /**
-     * @param int|string  $phone
-     * @param string      $content
+     * @param int|string  $recipient
+     * @param string      $message
      * @param string|null $sender
      *
      * @return string|false
      * @throws GuzzleException
      */
-    public function send(int|string $phone, string $content, string $sender = null): string|false
+    public function send(int|string $recipient, string $message, string $sender = null): string|false
     {
         if (!is_numeric($phone)) {
             return false;
         }
-        $response = $this->sendRequest('/send_sms', [
-            'destination' => $phone,
-            'body'        => base64_encode($content),
-            'sender'      => base64_encode($sender)
-        ]);
+        $params = [
+            'destination' => $recipient,
+            'body'        => base64_encode($message),
+        ];
+        if ($sender) {
+            $params['sender'] = base64_encode($sender);
+        }
+        $response = $this->sendRequest('/send_sms', $params);
         if (preg_match('~sms queued~i', $response) < 1) {
             throw new MessageNotEnqueuedError($response);
         }
